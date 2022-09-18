7e3f718ad038c4e651786c77646f4a9783902fb3
srsandy <sandeepranjan@yahoo.co.in>
2022-09-18 11:52:45 +0530

    feat: add postman collection

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import * as authController from "../controllers/auth.controllers.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 router.post("/signup", authController.signup);
 router.post("/register", authController.signup);
@@ -9,7 +10,7 @@ router.post("/register", authController.signup);
 router.post("/signin", authController.signin);
 router.post("/login", authController.signin);
 
-router.post("/signout", authController.signout);
-router.post("/logout", authController.signout);
+router.get("/signout", verifyToken, authController.signout);
+router.get("/logout", verifyToken, authController.signout);
 
 export default router;
